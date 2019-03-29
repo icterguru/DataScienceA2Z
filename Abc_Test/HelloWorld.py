02b3d84653750f05c4ac7836ade689f49da8a8ca
CSITEd Experts <csitedexperts@gmail.com>
2019-03-29 15:07:33 -0400

    Project cleaned

--- Abc_Test/HelloWorld.py
+++
@@ -1,5 +0,0 @@
-# https://www.w3schools.com/python/python_classes.asp
-
-hw = "Hello world!"
-for i in range(1, 5):
-    print (hw)
--- GSUCS6780/Homework2/Homework2aMatlab.m
+++
@@ -1,7 +0,0 @@
-clear all;
-close all;
-clc;
-randi([0, 0], 4, 10);
-% A = rand(4, 5)
-% plot.show();
-hold on;
